19b57cb467d61c2ec1b5f1f1c9aa66ba37747581
Juan Pablo <pablo.jp116@gmail.com>
2023-06-26 16:16:18 -0300

    Formatting HTML and creating a script

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const robotron = document.getElementById("robotron");
+
+robotron.addEventListener("click", () => {
+    console.log("Cliquei no robô");
+});
+
+function dizOi() {
+    console.log("Oi\nBem-vindo ao Robotron 2000");
+}
+
+dizOi();
